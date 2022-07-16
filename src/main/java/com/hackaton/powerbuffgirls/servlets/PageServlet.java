3e0a7a0a409acf315a827e8beeeb7768e3edd4f1
Samuel Santos <samuel.santos@feedzai.com>
2022-07-17 00:08:19 +0100

    WIP

--- src/main/java/com/hackaton/powerbuffgirls/servlets/PageServlet.java
+++ src/main/java/com/hackaton/powerbuffgirls/servlets/PageServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "PageServlet", urlPatterns = "/PowerBuffGirls")
+@WebServlet(name = "PageServlet", urlPatterns = "/PowerBuffGirls/deopressing")
 public class PageServlet extends HttpServlet {
 
     @Override
